aa1676ecf17ee4989401f7b1e4f6b5f83c9424dc
Jory Z. Ruscio <jzruscio@gmail.com>
2010-04-06 22:34:27 -0700

    Added unfollow capabilities

--- app/controllers/followees_controller.rb
+++ app/controllers/followees_controller.rb
@@ -3,19 +3,33 @@ class FolloweesController < ApplicationController
 
   def index
     if logged_in?
-      @user = current_user
-      @tweets = current_user.twitter.get('/statuses/friends')
-      @in = logged_in?
+      find_followees
     else
       redirect_to :controller => "sessions", :action => "new"
     end
   end
 
-#  def logout
-#    current_user = nil
-#    session[:user_id] = nil
-#    redirect_to_index("You have successfully logged out (controller)")
-#  end 
+  def unfollow
+    params.each do |key, value| 
+      if value == "on"
+        temp = "/friendships/destroy.xml?user_id=#{key}"
+        #logger.debug "**#{temp}**\n"
+        #begin
+        @resp= current_user.twitter.post(temp)
+        #rescue TwitterAuth::Dispatcher::Error => e
+        #  logger.info "**#{e}**\n"
+        #end
+      end
+    end
+    find_followees
+    redirect_to :action => "index"
+  end
+
+  def find_followees
+    @tweets = current_user.twitter.get('/statuses/friends')
+      @user = current_user
+      @in = logged_in?
+  end
 
   private
   def redirect_to_index(msg = nil)
